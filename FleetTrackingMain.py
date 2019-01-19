7de93c38aea5664f99351bb7a69975c35d8f378d
HeroicosHM <houghtonawe@gmail.com>
2019-01-19 00:42:34 -0700

    Added Google Cloud Platform API Keys Retreived basic API requests

--- FleetTrackingMain.py
+++ FleetTrackingMain.py
@@ -1,3 +1,14 @@
 import json
+import requests
+import time
+import asyncio
 
-print("Hello World!")
+async def mainLoop():
+    while True:
+        r = requests.get("http://52.160.46.169//api/EquipmentManagement/v1.0/EquipmentUnit", timeout = 10)
+        machineData = json.loads(r.text)
+        print(machineData)
+        await asyncio.sleep(30)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(mainLoop())
