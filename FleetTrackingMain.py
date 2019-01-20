bca8e203db0331e7647f79e0d25e42c04a038fe8
HeroicosHM <houghtonawe@gmail.com>
2019-01-20 07:07:10 -0700

    Revised fleet History tracking.

--- FleetTrackingMain.py
+++ FleetTrackingMain.py
@@ -2,7 +2,6 @@ import json
 import requests
 import time
 import pyrebase
-import websocket
 import threading
 import datetime
 
@@ -21,53 +20,24 @@ connectionConfig = {
 firebase = pyrebase.initialize_app(connectionConfig)
 db = firebase.database()
 
-websocketURL = CONFIG['WEBSOCKET_URL']
 api_url = CONFIG['API_URL']
-
-def onSocketUpdate(socket, message):
-    vehicleUpdateData = json.loads(message)
-    updateJSON = []
-    for i in range(0, len(vehicleUpdateData['payload'])):
-        vehicleUpdateData['payload'][i]['lastPosition']['longitude'] = vehicleUpdateData['payload'][i]['lastPosition']['longitude'] + 8
-        updateJSON.append({"id": vehicleUpdateData['payload'][i]['equipmentUnitId'],"update": {"lastPosition": {"longitude": vehicleUpdateData['payload'][i]['lastPosition']['longitude'], "latitude": vehicleUpdateData['payload'][i]['lastPosition']['latitude'], "elevation": vehicleUpdateData['payload'][i]['lastPosition']['elevation'], "speed": vehicleUpdateData['payload'][i]['lastPosition']['speed'], "heading": vehicleUpdateData['payload'][i]['lastPosition']['heading'], "timestamp": vehicleUpdateData['payload'][i]['lastPosition']['timestamp']}}})
-
-    for update in updateJSON:
-        db.child("Machines").child(update['id']).child('lastPosition').set(update['update']['lastPosition'])
-
-def onSocketError(sockket, error):
-    print(error)
-
-def onSocketClose(socket):
-    print("###### SOCKET CLOSING ######")
-
-def onSocketOpen(socket):
-    websocketSubscribeMessage = "ws://mine-view/subscribe?{'equipmentUnitIds':%s, 'boundingBox':{'minLatitude': -5, 'minLongitude': -84, 'maxLatitude': -3, 'maxLongitude': -82}}" % (machineIDS)
-    socket.send(websocketSubscribeMessage)
 
 def trackVehicleHistory():
     print("### STARTING TO STORE HISTORICAL DATA ###")
     while True:
+        try:
+            r = requests.get(api_url, timeout = 10)
+            machineData = json.loads(r.text)
+            print(machineData)
+
+            for machine in machineData:
+                machine['lastPosition']['longitude'] = machine['lastPosition']['longitude'] + 8
+                timestamp = machine['lastPosition']['timestamp'].split(".")[0]
+                db.child("MachineHistory").child(machine['id']).child(round(datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S").timestamp())).set(machine['lastPosition'])
+                db.child("Machines").child(machine['id']).set(machine)
+        except:
+            pass
         time.sleep(15)
-        r = requests.get(api_url, timeout = 10)
-        machineData = json.loads(r.text)
-        print(machineData)
-
-        for machine in machineData:
-            machine['lastPosition']['longitude'] = machine['lastPosition']['longitude'] + 8
-            machineIDS.append(machine['id'])
-            timestamp = machine['lastPosition']['timestamp'].split(".")[0]
-            db.child("MachineHistory").child(machine['id']).child(round(datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S").timestamp())).set(machine['lastPosition'])
-
-"""
-async def mainLoop(url):
-
-    async with websockets.connect(url) as socket:
-        await socket.send(websocketSubscribeMessage)
-        while True:
-            vehicleData = json.loads(await socket.recv())
-            for i in range(0, len(vehicleData['payload'])):
-                vehicleData['payload'][i]['lastPosition']['longitude'] = vehicleData['payload'][i]['lastPosition']['longitude'] + 8
-"""
 
 if __name__ == "__main__":
     r = requests.get(api_url, timeout = 10)
@@ -80,8 +50,3 @@ if __name__ == "__main__":
         db.child("Machines").child(machine['id']).set(machine)
 
     threading.Thread(target = trackVehicleHistory).start()
-
-    websocket.enableTrace(True)
-    socket = websocket.WebSocketApp(websocketURL, on_message = onSocketUpdate, on_error = onSocketError, on_close = onSocketClose)
-    socket.on_open = onSocketOpen
-    socket.run_forever()
